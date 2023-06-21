f60835e9c0bd9c992a77e9c3cc27f1f86631bf7b
hoyi <18087814+hoysics@users.noreply.github.com>
2023-06-21 19:16:18 +0800

    feat(concurrent_blocking_queue_v1): 阻塞队列依赖带缓存chan实现的版本 存在异常场景

--- queue/concurrent_blocking_queue.go
+++ queue/concurrent_blocking_queue.go
@@ -2,7 +2,6 @@ package queue
 
 import (
 	"context"
-	"errors"
 	"sync"
 )
 
@@ -12,6 +11,9 @@ type ConcurrentBlockingQueue[T any] struct {
 	lock sync.Mutex
 	data []any
 
+	notEmpty chan struct{}
+	notFull  chan struct{}
+
 	head int
 	tail int
 	size int
@@ -19,51 +21,80 @@ type ConcurrentBlockingQueue[T any] struct {
 
 func NewConcurrentBlockingQueue[T any](size int) *ConcurrentBlockingQueue[T] {
 	return &ConcurrentBlockingQueue[T]{
-		data: make([]any, size),
+		data:     make([]any, size),
+		notEmpty: make(chan struct{}, 1),
+		notFull:  make(chan struct{}, 1),
 	}
 }
 
 func (c *ConcurrentBlockingQueue[T]) Enqueue(ctx context.Context, t T) error {
-	if c.IsFull() {
-		return errors.New(`queue is full`)
-	}
 	c.lock.Lock()
-	defer c.lock.Unlock()
+	for c.IsFull() {
+		c.lock.Unlock()
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-c.notFull:
+			c.lock.Lock()
+		}
+	}
 	c.data[c.tail] = t
-	//c.tail = (c.tail + 1) % c.size
 	c.tail++
 	if c.tail == len(c.data) {
 		c.tail = 0
 	}
 	c.size++
+	// 如果有人在等待有数据 则应该唤醒这个人 但是不能阻塞我自己
+	if c.size == 1 {
+		// 只有从满变不满发信号
+		c.notEmpty <- struct{}{}
+	}
+	c.lock.Unlock()
 	return nil
 }
 
 func (c *ConcurrentBlockingQueue[T]) Dequeue(ctx context.Context) (t T, err error) {
-	if c.IsEmpty() {
-		err = errors.New(`queue is empty`)
-		return
-	}
 	c.lock.Lock()
-	defer c.lock.Unlock()
+	for c.IsEmpty() {
+		c.lock.Unlock()
+		// 注意这两步的时间差
+		select {
+		case <-ctx.Done():
+			err = ctx.Err()
+			return
+		case <-c.notEmpty:
+			c.lock.Lock()
+		}
+	}
 	t = c.data[c.head]
 	c.head++
-	//c.head = (c.head + 1) % c.size
 	if c.head == len(c.data) {
 		c.head = 0
 	}
 	c.size--
+	// 如果有人在等待少个数据 则应该唤醒这个人 但是不能阻塞我自己
+	if c.size == len(c.data)-1 {
+		//只有从空变不空发信号
+		c.notFull <- struct{}{}
+	}
+	c.lock.Unlock()
 	return
 }
 
 func (c *ConcurrentBlockingQueue[T]) IsEmpty() bool {
+	c.lock.Lock()
+	defer c.lock.Unlock()
 	return c.size == 0
 }
 
 func (c *ConcurrentBlockingQueue[T]) IsFull() bool {
+	c.lock.Lock()
+	defer c.lock.Unlock()
 	return c.size == len(c.data)
 }
 
 func (c *ConcurrentBlockingQueue[T]) Len() int {
+	c.lock.Lock()
+	defer c.lock.Unlock()
 	return c.size
 }
